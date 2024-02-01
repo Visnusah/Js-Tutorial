7666651e151fc5115f63458f766fc3239837aded
Kamlesh Sah <119692380+Visnusah@users.noreply.github.com>
2024-02-01 01:57:18 +0000

    ConversionOperationa

---
+++ 01_Basics/03_conversionOpration.js
@@ -0,0 +1,35 @@
+/* let RollNon = undefined
+
+console.log(typeof RollNon);
+console.log(typeof (RollNon));  
+
+let ValueInNumber = Number(RollNon)
+
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+*/
+
+// NaN --> NOt A Number --> It is Type of it's language it will be fixed after learned deep!                   
+ // 0 --> If you put undefined in let RollNon then output will be 0 it will also be fixed after some some when you will use library
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+/*
+
+let isLoggedIn = "Kamlesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1 => true; 0 => false
+// "" => false
+// "Kamlesh" => true
+
+let someNumber = 33
+
+let somestring = String(someNumber)
+
+console.log(typeof somestring); 
--- 01_Basics/03_test.js
+++
@@ -1,5 +0,0 @@
-let name = "guddu chaudahry"
-age = 23; accountId = 1233
-accountEmail = "duddu@gmail.com"
-
-console.table([name, age, accountId, accountEmail]);
