6913a2baa8689eeec848ac96a4e49095ef947291
Kamlesh Sah <119692380+Visnusah@users.noreply.github.com>
2024-02-02 00:13:19 +0000

    Cover basics comarision!

---
+++ 01_Basics/04_comaparision.js
@@ -0,0 +1,31 @@
+/* basics conversion */
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 < 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("kaml" > 2);
+
+
+
+// console.log(null > 0);   //false
+// console.log(null == 0); //falsa
+// console.log(null >= 0);  //true
+
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === use strictly and strict check, it is not only for check for values also check for datatype!!
+
+// console.log("2" === 2);
+console.log("Kamlesh" === 5);
