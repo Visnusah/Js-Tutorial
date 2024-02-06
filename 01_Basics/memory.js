3607321e759d9e333f820b0513f0257fe530a625
Kamlesh Sah <119692380+Visnusah@users.noreply.github.com>
2024-02-06 12:38:06 +0000

    Memory In JS

--- 01_Basics/memory.js
+++ 01_Basics/memory.js
@@ -17,13 +17,20 @@ console.log(anotherName); // ProgramProdigy
 // Heap
 
 
+// Define an object userOne with properties email and contact
 let userOne = {
-    email: "sahk@gmail.com",
-    contact: 98129293293,
-}
+    email: "sahk@gmail.com", // userOne's email
+    contact: 98129293293, // userOne's contact number
+};
 
-let userTwo = userOne
-userTwo.email = "Change@gmail.com"
+// Assign userOne to userTwo, so they both point to the same object
+let userTwo = userOne;
 
-console.log(userOne.email);  // Change@gmail.com
-console.log(userTwo.email); // Change@gmail.com
+// Change the email property of userTwo
+userTwo.email = "Change@gmail.com";
+
+// Log the email property of userOne, which has been changed through userTwo
+console.log(userOne.email); // Outputs: Change@gmail.com
+
+// Log the email property of userTwo
+console.log(userTwo.email); // Outputs: Change@gmail.com
