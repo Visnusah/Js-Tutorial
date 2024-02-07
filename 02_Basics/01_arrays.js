5727b03c0473f61718a7402a196c1e3a53004b3f
Kamlesh Sah <119692380+Visnusah@users.noreply.github.com>
2024-02-07 15:30:44 +0000

    Array

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,22 @@
+//Array
+
+const myArray = [83, 81, 123, 12]
+const myHeros = ["ironman", "spiderman", "antman"]
+
+const myArr2 = new Array (3, 5, 7, 9)
+//  console.log(myArray);
+//  console.log(myArray[0]);
+
+console.log(myArray.indexOf(8));
+
+ // Aerray methods
+
+// myArr2.push(11)
+// myArr2.push(13)
+// myArr2.pop()
+// myArr2.unshift(11)
+// myArr2.shift()
+
+// console.log(myArr2.includes(2));
+
+console.log(myArr2);
