d954334730d38012d26a370c7771da93fcd9b7f6
Visnusah <119692380+Visnusah@users.noreply.github.com>
2024-02-13 01:26:54 +0545

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,47 @@
+// console.log("K");
+// console.log("A");
+// console.log("M");
+// console.log("L");
+// console.log("E");
+// console.log("s");
+// console.log("H");
+
+console.log(" ");
+function Name(){
+    console.log("K");
+    console.log("A");
+    console.log("M");
+    console.log("L");
+    console.log("E");
+    console.log("s");
+    console.log("H");
+}
+
+// Name()
+
+function addTwonumber( num1, num2){
+    console.log(num1 + num2);
+}
+
+// addTwonumber(2,3)
+
+
+// const result = addTwonumber(6, 3)
+// console.log(result);
+
+// other method to declare a function
+
+function multiply(num1, num2){
+
+   let gunna = num1 * num2
+    return gunna
+}
+
+const gunna = multiply(2, 5)
+// console.log("Multiplication of two no: ", gunna);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("kamlesh"));
