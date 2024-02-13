92524a2c1eab1549bc9892970266fa131be9393e
Visnusah <119692380+Visnusah@users.noreply.github.com>
2024-02-13 20:58:00 +0545

    Function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -19,29 +19,57 @@ function Name(){
 
 // Name()
 
+//************** ****************************//
+
 function addTwonumber( num1, num2){
-    console.log(num1 + num2);
+    // console.log(num1 + num2);
 }
 
-// addTwonumber(2,3)
-
-
-// const result = addTwonumber(6, 3)
+addTwonumber(2,3)
+const result = addTwonumber(6, 3)
 // console.log(result);
 
+
+//************** ****************************//
 // other method to declare a function
 
 function multiply(num1, num2){
 
    let gunna = num1 * num2
     return gunna
+    console.log("kamlesh");
 }
 
-const gunna = multiply(2, 5)
-// console.log("Multiplication of two no: ", gunna);
+const abc = multiply(2, 5)
+// console.log("Multiplication of two no: ", abc);
+
+//************** ****************************//
+
+function addnumbers(number1, number2){
+    return number1 + number2
+}
+const output = addnumbers(2, 8);
+// console.log("result: ", output);
+
+//************** ****************************//
 
 function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("kamlesh"));
+// console.log(loginUserMessage("kamlesh"));
+
+//************** ****************************//
+
+function loginId(id){
+    if(id === undefined){
+        console.log("please enter ID of the user");
+        return
+    }
+    return `${id} is Logged In.`
+}
+
+// console.log(loginId());
+
+
+//************** ****************************//
