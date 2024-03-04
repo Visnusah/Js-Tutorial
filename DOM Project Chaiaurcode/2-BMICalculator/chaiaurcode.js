57b5dc7e743aa6b1c8259af832d1b35623cdc8c7
Visnusah <119692380+Visnusah@users.noreply.github.com>
2024-03-05 01:02:57 +0545

    project 02 BMI calculator

---
+++ DOM Project Chaiaurcode/2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    // const results = document.querySelector('#results'); 
+    const results = document.getElementById('results'); // both method is good
+    
+    
+    if(height === '' || height < 0 || isNaN(height)){ // height != NaN is a old technique
+        results.innerHTML = `<span>please give a valid height</span>`
+    } else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `<span>please give a valid weight</span>`
+    } else {
+        const bmi = (weight / (height * height / 10000)).toFixed(2)
+
+        //show output
+        results.innerHTML = `<span>Your BMI is ${bmi} </span>`
+
+        if (bmi < 18.6) {
+            results.innerHTML = `<span>Your BMI is ${bmi}<br>And you are under weight </span>`
+        } else if (bmi > 18.6 && bmi < 24.9){
+            
+            results.innerHTML = `<span>Your BMI is ${bmi} <br>And you are normal. </span>`
+        } else {
+            results.innerHTML = `<span>Your BMI is ${bmi} <br>And you are overweight. </span>`
+        }
+        
+    }
+    
+})
