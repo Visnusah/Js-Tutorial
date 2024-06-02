d9dda67193c214faf9a73b64a003f5c4fa2af510
kamlesh sah <sahk5858@gmail.com>
2024-06-02 10:57:30 +0545

    Behind the scene in class in js

--- 10_clases_and_Oop/myclasses.js
+++ 10_clases_and_Oop/myclasses.js
@@ -6,13 +6,42 @@ class user {
     this.password = password;
   }
   encryptPassword(){
-    // return `${this.password}abc`
-    this.password = this.password.split("").map((char) => {
-      return char.charCodeAt(0) + 1;
-    });
+    // this.password = this.password.split("").map((char) => {
+    //   return String.fromCharCode(char.charCodeAt(0) + 1);
+    // }).join(""); // Convert array back to string
+    
+    return `${this.password}`
   }
+  changeUsername(){
+    return `${this.username.toUpperCase()}`
+}
+  
 }
 
 const kamlesh = new user("kamlesh", "kamlesh@gmail.com", "#$#54354kamlesh");
-console.log(kamlesh.encryptPassword());
-console.log(kamlesh); // password is encrypted
+console.log(kamlesh.encryptPassword()); // Encrypt the password
+console.log(kamlesh.changeUsername()); // Change the username to uppercase
+console.log(kamlesh); // Now the password is encrypted and the object is logged correctly
+
+
+
+// behind the scene with another example
+
+function User01(username, email, password){
+  this.username = username;
+  this.email = email;
+  this.password = password;
+  
+}
+
+
+User01.prototype.encryptPassword = function(){
+  return `${this.password}abc` // Encrypt the password
+};
+User01.prototype.changeUsername = function(){
+  return `${this.username.toUpperCase()}` // Convert the username to uppercase
+};
+const info = new User01("kamlesh", "Example@google.com", "%#$%#%$ksksks");
+
+console.log(info.encryptPassword()); //  Encrypt the password
+console.log(info.changeUsername()); // Change the username to uppercase
